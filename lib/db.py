8da9f8b06bb127d4ebd0b5c6fe04882a83702d38
maranimitchelle@gmail.com <mitch@DESKTOP-PMCR53J.localdomain>
2025-10-16 10:37:41 +0300

    Created all models

--- lib/db.py
+++ lib/db.py
@@ -10,3 +10,6 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
+
+def get_session():
+    return SessionLocal()
